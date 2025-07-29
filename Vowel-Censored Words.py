b78638e78de3a0e60442f21305f5523b873dc85d
gavin.peterson <gpeterson.ce@gmail.com>
2025-07-29 14:47:45 -0600

    new file

---
+++ Vowel-Censored Words.py
@@ -0,0 +1,19 @@
+# Write a function that takes a list of words and returns a new
+# list containing only the words that contain more vowels than consonants.
+
+def more_vowels_than_consonants(word_list):
+    # TODO: Use a list comprehension to filter words
+    vowel_count = 0
+    consonant_count = 0
+
+    result = [
+        # your code here
+    ]
+    for word in word_list:
+        if
+    return result
+
+
+# Test
+words = ["hello", "eye", "try", "automobile", "sky", "aeiou"]
+print(more_vowels_than_consonants(words))  # ➞ ['eye', 'automobile', 'aeiou']
